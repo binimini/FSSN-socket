3d481f4e800f73441aa0250e7bd5be4d72cff678
Subin Min <tnqls5417@naver.com>
2022-06-03 02:21:39 +0900

    feat: TCP chat server (net) 구현

--- 1-tcp-echo-server.js
+++ 1-tcp-echo-server.js
@@ -1,30 +1,28 @@
-const net = require("net");
+const net = require('net');
 
-const HOST = "127.0.0.1";
+const HOST = '127.0.0.1';
 const PORT = 65456;
 
 const server = net.createServer((client) => {
-  client.on("data", (data) => {
+  client.on('data', (data) => {
     console.log(`> echoed: ${data}`);
     client.write(data);
 
-    if (data == "quit") {
+    if (data == 'quit') {
       server.close();
     }
   });
 });
 
 server.listen(PORT, HOST, () => {
-  console.log("> echo-server is activated");
+  console.log('> echo-server is activated');
 });
 
-server.on("connection", (client) => {
+server.on('connection', (client) => {
   console.log(
-    `> client connected by IP address ${
-      client.address().address
-    } with Port number ${client.address().port}`
+    `> client connected by IP address ${client.remoteAddress} with Port number ${client.remotePort}`
   );
 });
-server.on("close", () => {
-  console.log("> echo-server is de-activated");
+server.on('close', () => {
+  console.log('> echo-server is de-activated');
 });
--- 5-tcp-chat-server.js
+++ 5-tcp-chat-server.js
@@ -1,15 +1,27 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const net = require('net');
 
 const HOST = '127.0.0.1';
 const PORT = 65456;
+const group_queue = new Set();
 
 const server = net.createServer((client) => {
   client.on('data', (data) => {
-    console.log(`> echoed: ${data}`);
-    client.write(data);
-
     if (data == 'quit') {
-      server.close();
+      group_queue.delete(client);
+      client.write(data);
+    } else {
+      console.log(
+        `> received (${data}) and echoed to ${group_queue.size} clients`
+      );
+      for (let cli of group_queue) {
+        cli.write(data);
+      }
     }
   });
   client.on('error', () => {
@@ -27,6 +39,7 @@ server.listen(PORT, HOST, () => {
 });
 
 server.on('connection', (client) => {
+  group_queue.add(client);
   console.log(
     `> client connected by IP address ${client.remoteAddress} with Port number ${client.remotePort}`
   );
@@ -34,9 +47,23 @@ server.on('connection', (client) => {
 
 server.on('close', () => {
   console.log('> echo-server is de-activated');
+  rl.close();
 });
 
 server.on('error', () => {
   console.log('> server socket error and program terminated');
   server.close();
+});
+
+rl.on('line', (line) => {
+  if (line == 'quit') {
+    if (group_queue.size == 0) {
+      console.log('> stop procedure started');
+      server.close();
+    } else {
+      console.log(
+        `> active clients are remained : ${group_queue.size} clients`
+      );
+    }
+  }
 });
